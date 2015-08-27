cbdca9756f1f2ba476659b4cea15bad32ebb489b
Yujiro Kato <wakuwakuwaffurunrun@outlook.jp>
2015-08-27 15:59:08 +0900

    とりあえず標準コントロールの各テーマは完了？Sliderに一部バグあり。

--- YKToolkit.Sample/ViewModels/StandardControls/RepeatButtonViewModel.cs
+++ YKToolkit.Sample/ViewModels/StandardControls/RepeatButtonViewModel.cs
@@ -1,6 +1,47 @@
 ﻿namespace YKToolkit.Sample.ViewModels
 {
+    using YKToolkit.Bindings;
+
     public class RepeatButtonViewModel : ViewModelBase
     {
+        private int _count;
+        /// <summary>
+        /// カウント値を取得または設定します。
+        /// </summary>
+        public int Count
+        {
+            get { return _count; }
+            set { SetProperty(ref _count, value); }
+        }
+
+        private DelegateCommand _increaseCommand;
+        /// <summary>
+        /// カウント加算コマンドを取得します。
+        /// </summary>
+        public DelegateCommand IncreaseCommand
+        {
+            get
+            {
+                return _increaseCommand ?? (_increaseCommand = new DelegateCommand(_ =>
+                {
+                    Count++;
+                }));
+            }
+        }
+
+        private DelegateCommand _resetCommand;
+        /// <summary>
+        /// カウントリセットコマンドを取得します。
+        /// </summary>
+        public DelegateCommand ResetCommand
+        {
+            get
+            {
+                return _resetCommand ?? (_resetCommand = new DelegateCommand(_ =>
+                {
+                    Count = 0;
+                }));
+            }
+        }
     }
 }
