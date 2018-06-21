4df7bf42835a8db10b7f70f81db56a977f2dc672
yu-katou <yu-katou@shibuya.co.jp>
2018-06-21 14:56:32 +0900

    QueryPerformanceCounter を使用した QpTimer クラスを追加。

---
+++ YKToolkit.Controls/Core/QpTimer.cs
@@ -0,0 +1,80 @@
+﻿namespace YKToolkit.Controls.Core
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// QueryPerformanceCounter による時間計測タイマーを表します。
+    /// </summary>
+    public class QpTimer
+    {
+        [DllImport("Kernel32.dll")]
+        private static extern bool QueryPerformanceCounter(out long lpPerformanceCount);
+        [DllImport("Kernel32.DLL")]
+        private static extern bool QueryPerformanceFrequency(out long lpFrequency);
+
+        /// <summary>
+        /// 静的なコンストラクタ
+        /// </summary>
+        static QpTimer()
+        {
+            if (!QueryPerformanceFrequency(out _frequency))
+                throw new Exception("高解像力パフォーマンスカウンタがサポートされていません。");
+        }
+
+        /// <summary>
+        /// 新しいインスタンスを生成します。
+        /// </summary>
+        public QpTimer()
+        {
+            Start();
+        }
+
+        /// <summary>
+        /// 時間計測を開始します。
+        /// </summary>
+        public void Start()
+        {
+            QueryPerformanceCounter(out this._start);
+        }
+
+        private static long _frequency;
+        /// <summary>
+        /// 1 秒あたりのカウント数を取得します。
+        /// </summary>
+        public static long Frequency
+        {
+            get { return _frequency; }
+            private set { _frequency = value; }
+        }
+
+        /// <summary>
+        /// 経過時間をミリ秒単位で取得します。
+        /// </summary>
+        public double MilliSeconds
+        {
+            get
+            {
+                var end = 0L;
+                QueryPerformanceCounter(out end);
+                return (end - this._start) * 1000.0 / Frequency;
+            }
+        }
+
+        /// <summary>
+        /// 経過時間を秒単位で取得します。
+        /// </summary>
+        public double Seconds
+        {
+            get
+            {
+                return this.MilliSeconds / 1000.0;
+            }
+        }
+
+        /// <summary>
+        /// 時間計測開始時のカウント数
+        /// </summary>
+        private long _start;
+    }
+}
--- YKToolkit.Controls/Properties/AssemblyInfo.cs
+++ YKToolkit.Controls/Properties/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.17.0")]
+[assembly: AssemblyVersion("2.0.18.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
