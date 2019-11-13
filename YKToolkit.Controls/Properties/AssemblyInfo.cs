3152507117b3b85e5a161b2e5f77f6b289ffd647
YKSoftware <wakuwakuwaffurunrun@outlook.jp>
2019-11-13 13:39:18 +0900

    Ver.2.3.4 β rev.2。
    DataGridClumnHeaderStyle の HrizontalContentAlignment の既定値を Stretch に変更し、
    ヘッダコンテンツの中央揃えを簡単におこなえるようにしました。

--- YKToolkit.Controls/Properties/AssemblyInfo.cs
+++ YKToolkit.Controls/Properties/AssemblyInfo.cs
@@ -18,4 +18,4 @@ using System.Windows;
 [assembly: ComVisible(false)]
 [assembly:ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("2.3.4.1")]
+[assembly: AssemblyVersion("2.3.4.2")]
